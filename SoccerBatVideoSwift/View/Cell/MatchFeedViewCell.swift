69a43ea029775beaffbb05ee69d95d8caa5842a0
sungyoungPark <sygitter1491012@gmail.com>
2024-07-11 01:15:12 +0900

    cell ui 그리기 작업

---
+++ SoccerBatVideoSwift/View/Cell/MatchFeedViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MatchFeedViewCell.swift
+//  SoccerBatVideoSwift
+//
+//  Created by 박성영 on 7/10/24.
+//
+
+import UIKit
+
+class MatchFeedViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
